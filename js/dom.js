56dc27af1a1ef8a3ca5fcfd5f9453e4dbfda8d7a
Gilberto Malaga <fullgamer494@hotmail.com>
2025-09-29 03:33:25 -0600

    Update api.js to request FBI most wanted data

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,12 @@
+// Access screen (login)
 const accessScreen = document.getElementById('access-screen')
 const agentNum = document.getElementById('agentInput')
 const welcomeAgentNum = document.getElementById('agent-num')
 
+
+// Home quotes
 const qText = document.getElementById('quote-text')
 const qAuthor = document.getElementById('quote-author')
+
+// Missions criminals grid
+const crimiGrid = document.getElementById('criminals-grid')
---
+++ js/missions.js
@@ -0,0 +1,24 @@
+async function main(){
+    const data = await getAPIData()
+    const most_wanted = data.wanted.items
+
+    console.log(most_wanted)
+
+    most_wanted.forEach((element) => {
+        crimiGrid.innerHTML += `
+            <div class="criminal-card">
+                <div class="criminal-img">
+                    <img src="${element.images[0].original != most_wanted[0].images[0].original && element.images[0].original != most_wanted[1].images[0].original && element.images[0].original != most_wanted[11].images[0].original? element.images[0].original : '../src/most-wanted-default.jpg'}" alt="">
+                </div>
+                <div class="criminal-info">
+                        <h2>${element.title}</h2>
+                        <p>${element.details != null? element.details.slice(0, 150) + "..." : "Be careful, agent, this son of a bitch is very dangerous and elusive. We haven't been able to get many details on the bastard."}</p>
+                    </div>
+            </div>
+        `
+    });
+    
+
+}
+
+main()
