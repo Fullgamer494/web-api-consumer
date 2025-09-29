1f6bd21db0904333f7090e0a4b949a4d3e2252e4
Gilberto Malaga <fullgamer494@hotmail.com>
2025-09-29 12:09:05 -0600

    Add secret-identiy.js, print data API on HTML

---
+++ js/secret-identity.js
@@ -0,0 +1,33 @@
+async function main(){
+    const data = await getAPIData()
+
+    console.log(data.person_data.results[0])
+    profileContainer.innerHTML = `
+        <div class="new-identity-card">
+            <div class="new-identity-info">
+                <div class="info-column">
+                    <h2>Fullname:</h2>
+                    <span>${data.person_data.results[0].name.first + ' ' + data.person_data.results[0].name.last}</span>
+                    <h2>Age:</h2>
+                    <span>${data.person_data.results[0].dob.age}</span>
+                    <h2>Gender:</h2>
+                    <span>${data.person_data.results[0].gender}</span>    
+                </div>
+                <div class="info-column">
+                    <h2>Email:</h2>
+                    <span>${data.person_data.results[0].email}</span>
+                    <h2>Address:</h2>
+                    <span>${data.person_data.results[0].location.country + ', ' + data.person_data.results[0].location.state}</span>
+                    <h2>Phone:</h2>
+                    <span>${data.person_data.results[0].phone}</span>
+                </div>
+            </div>
+            <div class="new-identity-img">
+                <img src="${data.person_data.results[0].picture.large}" alt="">
+            </div>
+        </div>
+    `
+
+}
+
+main()
